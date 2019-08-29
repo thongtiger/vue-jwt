a6fa74d88ef1bc3bfdd74852cf6e4b9acc5e7559
thongtiger <thong_develop@gmail.com>
2019-08-29 14:58:42 +0700

    config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,22 @@
 // vue.config.js
 module.exports = {
     // options...
+    publicPath: '/',
+    assetsDir: 'static',
+    outputDir: 'app',
+    crossorigin:"anonymous",
     devServer: {
-          proxy: 'http://localhost:3000',
+        //  https: true ,
+        port: 8000,
+          proxy: {
+            '^/api': {
+              target: 'http://localhost:3000',
+              ws: true,
+              changeOrigin: true
+            },
+            '^/foo': {
+              target: 'http://localhost:3000'
+            }
+          }
       }
   }

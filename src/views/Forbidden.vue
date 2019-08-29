8619323e5b51a44ea5cf4a00196fbfad906e90f1
thongtiger <thong_develop@gmail.com>
2019-08-29 14:56:26 +0700

    access denied

---
+++ src/views/Forbidden.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Error 403</h2>
+        <p>Access Denied</p>
+    </div>
+</template>
--- src/views/User.vue
+++ src/views/User.vue
@@ -18,15 +18,15 @@
       </thead>
       <tbody>
         <tr v-for="(item,k) in items" :key="k">
-          <td>{{ item.id  }}</td>
-          <td>{{ item.username  }}</td>
-          <td>{{ item.nick_name  }}</td>
-          <td>{{ item.first_name  }}</td>
-          <td>{{ item.last_name  }}</td>
-          <td>{{ item.create_by  }}</td>
-          <td>{{ item.limit.max_order  }}</td>
-          <td>{{ item.limit.money_per_day  }}</td>
-          <td>{{ item.role  }}</td>
+          <td>{{ item.id }}</td>
+          <td>{{ item.username }}</td>
+          <td>{{ item.nick_name }}</td>
+          <td>{{ item.first_name }}</td>
+          <td>{{ item.last_name }}</td>
+          <td>{{ item.create_by }}</td>
+          <td>{{ item.limit.max_order }}</td>
+          <td>{{ item.limit.money_per_day }}</td>
+          <td>{{ item.role }}</td>
         </tr>
       </tbody>
     </table>
@@ -36,34 +36,50 @@
 <script>
 import store from "../store";
 // @ is an alias to /src
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "user",
-  data(){
-    return{
-      items:Array,
-      showCreateUserComponent: false,
-    }
+  data() {
+    return {
+      items: Array,
+      showCreateUserComponent: false
+    };
   },
-  components: {
-
-  },
+  components: {},
   methods: {
-    findAll(){
-       axios.get(store.state.api_url + "/user", { headers: { 'Authorization': `Bearer ${localStorage.getItem("access_token")}`,'Accept':`application/json`
-       }})
-        .then(response =>{
-          this.items = response.data
+    findAll() {
+      axios
+        .get(store.state.api_url + "/user", {
+          headers: {
+            Accept: `application/json;`
+          }
+        })
+        .then(response => {
+          this.items = response.data;
           // console.log(response.data)
           // if(response.data.success){
-            
+
           // }
         })
-        .catch(err => console.log(err));
+        .catch(error => {
+          // Error 😨
+          if (error.response) {
+            /*
+             * The request was made and the server responded with a
+             * status code that falls out of the range of 2xx
+             */
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            if (error.response.status == 403) {
+              this.$router.push('/forbidden')
+            }
+          }
+        });
     }
   },
-  mounted(){
-    this.findAll()
+  mounted() {
+    this.findAll();
   }
 };
 </script>
